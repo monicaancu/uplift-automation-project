a8d7fdc2eba3a1c69b7e3c3bdce1d1874932cf22
monicaancu <monica.ancu@gmail.com>
2023-03-06 22:03:11 -0500

    CukesRunner with the json plugin - removed the space

--- src/test/java/com/uplift/runners/CukesRunner.java
+++ src/test/java/com/uplift/runners/CukesRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "json: target/cucumber.json",
+                "json:target/cucumber.json",
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/uplift/step_definitions",
         dryRun = false,
-        tags = "not @dont_run",
+        tags = "",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
