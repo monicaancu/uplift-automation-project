4e16a5d500da986d971c8ec566913bb3b3502aa2
monicaancu <monica.ancu@gmail.com>
2023-03-07 10:44:58 -0500

    US 05 test cases by Monica

--- src/test/java/com/uplift/pages/PointOfSalePage.java
+++ src/test/java/com/uplift/pages/PointOfSalePage.java
@@ -1,6 +1,8 @@
 package com.uplift.pages;
 
 import com.uplift.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class PointOfSalePage {
@@ -8,4 +10,24 @@ public class PointOfSalePage {
     public PointOfSalePage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//a[@data-menu-xmlid='point_of_sale.menu_point_root']")
+    public WebElement pointOfSaleNavigationLink;
+
+    @FindBy(linkText = "Orders")
+    public WebElement ordersSideLink;
+
+    @FindBy(xpath = "//thead//input[@type='checkbox']")
+    public WebElement ordersTableCheckbox;
+
+    //this is what you click to open the dropdown
+    @FindBy(xpath = "//button[normalize-space()='Action']")
+    public WebElement actionsDropdownButton;
+
+    //this dropdown created with ul/li
+    @FindBy(xpath = "//div[@class='btn-group o_dropdown open']//ul[@role='menu']")
+    public WebElement actionsDropdownOptions;
+
+    @FindBy(xpath = "//tbody//div/input")
+    public WebElement orderCheckboxes;
 }
