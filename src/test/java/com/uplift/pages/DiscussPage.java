2849f3c8ae78790a3f0473be9489e6a926756265
snoopy3200 <denys_l20@yahoo.com>
2023-03-07 01:05:06 -0600

    Denys us13

--- src/test/java/com/uplift/pages/DiscussPage.java
+++ src/test/java/com/uplift/pages/DiscussPage.java
@@ -1,6 +1,8 @@
 package com.uplift.pages;
 
 import com.uplift.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DiscussPage {
@@ -8,4 +10,8 @@ public class DiscussPage {
     public DiscussPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//span[@class='oe_topbar_name']")
+    public WebElement loginName;
 }
+
